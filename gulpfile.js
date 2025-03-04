8ea53925a065384d765643010aedb1478e6b1e6f
Heclypso <heclypso@gmail.com>
2025-03-04 11:45:19 -0300

    feat: removido a tarefa comentada gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -33,12 +33,6 @@ function comprimeJavaScript(){
     .pipe(gulp.dest('dist/scripts'))
 }
 
-// function gulpWatch() {
-//   return watch('./src/styles/*.less', {ignoreInitial: false}, gulp.series(compilaLess)), 
-//   watch('./src/*.html', {ignoreInitial: false}, gulp.series(criaReplace, replaceTemplate, apagaReplace)),
-//   watch('./src/scripts/*.js', {ignoreInitial: false}, gulp.series(comprimeJavaScript));
-// }
-
 function comprimeImagens(callback) {
   gulp.src('./src/images/*')
   .pipe(imagemin())
